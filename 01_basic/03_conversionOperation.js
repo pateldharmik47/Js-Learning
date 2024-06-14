6fc910a4857ccb64ae9336fc4f0ad7352b329cfa
Dharmik Patel <pateldharmik4701@gmail.com>
2024-06-14 09:43:41 +0530

    Stcak and heap concepts

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -32,3 +32,39 @@ String:
 5 => string
 
 */
+
+
+/* 
+
+    Stack and Heap :
+    
+    Stack (Premative)  and Heap (Non Premative)
+    Premative: Number , boolean , string all are Premative.
+    Non Premative : Array , Object and Function are Non Premative. 
+
+    Other than ARRAY , OBJECT AND FUNCTION ALL ARE PREMATIVE TYPE.
+
+    Stack create a copy of memory and Heap create refrense for that variable.
+    
+*/
+
+let Name = "Dharmik Patel";
+let anotherName = Name;
+
+anotherName = "Shivam Ramani"
+
+console.log("Name=",Name); // outPut : Dharmik Patel (Stack)
+console.log("anotherName=",anotherName); // outPut : Shivam Ramani (Stack);
+
+let obj = {
+    email : "abc@test.com",
+    name : "ABC"
+}
+
+let obj2 = obj;
+
+obj2.email = "dharmik@learn.com";
+
+
+console.log("obj=",obj);
+console.log("obj2=",obj2);
